0f2da693a113f49fa99af88b26c55edebe9236d2
Vadim <cvi-vadim@yandex.ru>
2022-06-30 13:48:41 +0300

    helpers log

--- weather-app-cli/app.js
+++ weather-app-cli/app.js
@@ -1,1 +1,21 @@
-console.log("Weather app")
+#!/ust/bin/env node
+import {getArgs} from './helpers/args.js'
+import {printHelp} from "./services/log.services.js";
+
+
+const initCLI = () => {
+    const args = getArgs(process.argv)
+    if (args.h) {
+        printHelp()
+        // Вывод help
+    }
+    if (args.s) {
+        // сохранить город
+    }
+    if (args.t) {
+        // сохранить токен
+    }
+    // вывести погоду
+}
+
+initCLI()
