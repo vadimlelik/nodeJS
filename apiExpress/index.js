32be3ffc2d0a3a52dcc94d9afd644b094b2597b3
vadim.cvirko <promiti90@gmail.com>
2022-07-11 12:59:45 +0300

    init api express

---
+++ apiExpress/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const port = 8000
+const app = express()
+
+
+
+app.get('/hello', (req, res) => {
+	res.send('/Hello ')
+})
+
+
+app.listen(port, () => {
+	console.log('Server has started ' + port);
+})
+
