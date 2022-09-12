9b7fccce2456363cc521c0fd8d5e9a7bb2fb9c0d
vadim.cvirko <promiti90@gmail.com>
2022-09-12 15:18:04 +0300

    metadata reflection

--- rest-api/dist/main.js
+++ rest-api/dist/main.js
@@ -1,23 +1,18 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.appContainer = exports.app = void 0;
 const app_1 = require("./app");
 const exetpionFilter_1 = require("./errors/exetpionFilter");
 const logger_service_1 = require("./logger/logger.service");
 const users_controler_1 = require("./users/users.controler");
-function bootstrap() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const logger = new logger_service_1.LoggerService();
-        const app = new app_1.App(logger, new users_controler_1.UserController(logger), new exetpionFilter_1.ExaptionFilter(logger));
-        yield app.init();
-    });
-}
-bootstrap();
+const inversify_1 = require("inversify");
+const types_1 = require("./types");
+const appContainer = new inversify_1.Container();
+exports.appContainer = appContainer;
+appContainer.bind(types_1.TYPES.ILogger).to(logger_service_1.LoggerService);
+appContainer.bind(types_1.TYPES.ExaptionFilter).to(exetpionFilter_1.ExaptionFilter);
+appContainer.bind(types_1.TYPES.UserController).to(users_controler_1.UserController);
+appContainer.bind(types_1.TYPES.Application).to(app_1.App);
+const app = appContainer.get(types_1.TYPES.Application);
+exports.app = app;
+app.init();
